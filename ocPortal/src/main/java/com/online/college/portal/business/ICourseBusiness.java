13b6dc174eb49fadfce057b4b6b26f1df91bc462
liuliuliu <1240962938@qq.com>
2017-09-05 15:01:34 +0800

    index carousel classify

---
+++ ocPortal/src/main/java/com/online/college/portal/business/ICourseBusiness.java
@@ -0,0 +1,14 @@
+package com.online.college.portal.business;
+
+import com.online.college.portal.vo.CourseSectionVO;
+
+import java.util.List;
+
+public interface ICourseBusiness {
+
+	/**
+	 * 获取课程章节
+	 */
+	List<CourseSectionVO> queryCourseSection(Long courseId);
+	
+}
--- ocPortal/src/main/java/com/online/college/portal/controller/PortalController.java
+++ ocPortal/src/main/java/com/online/college/portal/controller/PortalController.java
@@ -1,19 +1,43 @@
 package com.online.college.portal.controller;
 
+import com.online.college.core.consts.domain.ConstsSiteCarousel;
+import com.online.college.core.consts.service.IConstsSiteCarouselService;
+import com.online.college.portal.business.IPortalBusiness;
+import com.online.college.portal.vo.ConstsClassifyVO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.List;
+
 @Controller
 @RequestMapping()
 public class PortalController {
-    /*
-    首页
-     */
+
+    @Autowired
+    private IConstsSiteCarouselService constsSiteCarouselService;
+
+    @Autowired
+    private IPortalBusiness portalBusiness;
+
+    //首页
     @RequestMapping("/index")
     public ModelAndView index(){
         ModelAndView modelAndView = new ModelAndView("index");
-        modelAndView.addObject("info","这是pc首页");
+
+        //加载轮播
+        List<ConstsSiteCarousel> carouselList = constsSiteCarouselService.queryCarousels(4);
+        modelAndView.addObject("carouselList",carouselList);
+
+
+        //课程分类(一级分类）
+        List<ConstsClassifyVO> classifys = portalBusiness.queryAllClassify();
+
+        //课程推荐
+        portalBusiness.prepareRecomdCourses(classifys);
+        modelAndView.addObject("classifys", classifys);
+
         return modelAndView;
     }
 
